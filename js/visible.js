bdbb8252ad8f94f88fc744fe80dc70e218fd1011
QuantumPhi <phi.quantum@gmail.com>
2014-07-25 00:58:38 +0200

    Added docs

--- js/visible.js
+++ js/visible.js
@@ -1,20 +1,34 @@
 (function($) {
     var window = $(window);
 
-    $.fn.visible = function(partial) {
+    /*'partial' determines whether or not an object will be considered
+    as 'visible' if only part of the object is visible*/
+    $.fn.visible = function(partial, index) {
+        /*Don't bother processing if no such element exists*/
         if(this.length < 1)
             return;
 
-        var object = this.eq(0),
+        /*If 'partial' is undefined, set 'partial' to false*/
+        if(partial === undefined)
+            partial = false;
+
+        /*If 'index' is undefined, set 'index' to 0*/
+        if(index === undefined)
+            index = 0;
+
+        /*Grab specified element from fetched list*/
+        var object = this.eq(index),
             viewportH = window.height(),
             viewportW = window.width(),
 
+        /*Top left and bottom right coordinates*/
         var coord = [
             [object.position().left, object.position().top],
             [object.position().left + object.width(),
                 object.position().top + object.height()]
         ];
 
+        /*Basic rectangular boundary checking*/
         return (partial : coord[1][0] ? coord[0][0] < 0 || partial : coord[0][0] ? coord[1][0] >= viewportW)
             || (partial : coord[1][1] ? coord[0][1] < 0 || partial : coord[0][1] ? coord[1][1] >= viewportH);
     };
