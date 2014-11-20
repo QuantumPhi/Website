b4ef0a6d083c66c5a119ec0e4ef328f098b7ebb9
Tarik Onalan <phi.quantum@gmail.com>
2014-11-20 11:16:27 -0800

    Lots of stuff

---
+++ js/index.js
@@ -0,0 +1,8 @@
+require.config({
+    baseUrl: "lib/"
+})
+
+require(["jquery-1.11.1.min.js", "d3.min.js"], function($, d3) {
+    var width  = $(window).width()
+        height = $(window).height()
+})
