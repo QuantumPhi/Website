f794d048f92f55c8983d1116e37c089ba6b481fd
Tarik Onalan <phi.quantum@gmail.com>
2014-11-24 19:56:41 -0800

    Big changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,53 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        
+        pkg: grunt.file.readJSON("package.json"),
+
+        concat: {
+            options: {
+                stripBanners: true
+            },
+            dist: {
+                src: [
+                    "bower_components/d3/d3.min.js",
+                    "bower_components/jquery/dist/jquery.min.js",
+                    "bower_components/require/build/require.min.js"
+                ],
+                dest: "js/libs.js"
+            }
+        },
+
+        jade: {
+            compile: {
+                files: {
+                    "index.html" : "templates/index.jade"
+                }
+            }
+        },
+
+        uglify: {
+            dist: {
+                src: "<%= concat.dist.dest %>",
+                dest: "<%= concat.dist.dest %>"
+            }
+        },
+
+        watch: {
+            html: {
+                files: ["*.jade", "*.layout.jade"],
+                tasks: ["jade"]
+            },
+
+            scripts: {
+                files: ["bower_components/**/*.min.js"],
+                tasks: ["concat", "uglify"]
+            }
+        }
     })
+
+    grunt.loadNpmTasks("grunt-contrib-concat")
+    grunt.loadNpmTasks("grunt-contrib-jade")
+    grunt.loadNpmTasks("grunt-contrib-uglify")
+    grunt.loadNpmTasks("grunt-contrib-watch")
+
+    grunt.registerTask("default", ["concat", "jade", "uglify"])
 }
