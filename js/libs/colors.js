e467ef1b6ea8aba81830b53302e366a2f790d250
Tarik Onalan <phi.quantum@gmail.com>
2015-01-01 09:17:05 -0800

    Browserify

--- js/libs/colors.js
+++
@@ -1,8 +0,0 @@
-define(['jquery'], function($) {
-    return $.ajax({
-        url: 'https://cdn.rawgit.com/ozh/github-colors/master/colors.json',
-        cache: true,
-        async: false,
-        success: function(data) { return data }
-    })
-})
--- js/libs/repos.js
+++
@@ -1,8 +0,0 @@
-define(['jquery'], function($) {
-    return $.ajax({
-        url: 'https://api.github.com/users/QuantumPhi/repos?type=all&per_page=100',
-        cache: true,
-        async: false,
-        success: function(data) { return data }
-    })
-})
