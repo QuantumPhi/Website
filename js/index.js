3cbb2f06895fe5ced2b217f492db3aea97144613
Tarik Onalan <phi.quantum@gmail.com>
2015-01-15 04:43:54 -0800

    Added todo comment

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ var $      = require('jquery'),
 var width  = $(window).width(),
     height = $(window).height()
 
+// TODO: Create pagewalker
 var repos  = $.ajax({
     url: 'https://api.github.com/users/QuantumPhi/repos?type=all&per_page=100',
     cache: true,
